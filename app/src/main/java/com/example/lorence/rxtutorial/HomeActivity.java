680070a84ad81a33e2d7712349e2c7b00dedaa5d
lorence <lorence@enclave.vn>
2018-05-21 15:28:33 +0700

    Rx-5

--- app/src/main/java/com/example/lorence/rxtutorial/HomeActivity.java
+++ app/src/main/java/com/example/lorence/rxtutorial/HomeActivity.java
@@ -2,6 +2,16 @@ package com.example.lorence.rxtutorial;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.Flowable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,7 +20,30 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        Flowable<List<Node>> flowable = Flowable.fromArray(testGetNodes());
+        /**
+         * - subscribeOn:
+         * + Register Asynchronously
+         * + Parameter is passed
+         * ++ Schedulers:
+         */
+        flowable.subscribeOn(Schedulers.io()).
+                observeOn(AndroidSchedulers.mainThread()).
+                subscribe(new Consumer<List<Node>>() {
 
+                    @Override
+                    public void accept(@NonNull List<Node> nodes) throws Exception {
+                        Log.i("TAG", nodes.get(0).getName());
+                    }
+        });
+    }
 
+    private List<Node> testGetNodes() {
+        ArrayList<Node> list = new ArrayList<>();
+        list.add(new Node(1, "Math"));
+        list.add(new Node(2, "English"));
+        list.add(new Node(3, "Geography"));
+        list.add(new Node(4, "History"));
+        return list;
     }
 }
