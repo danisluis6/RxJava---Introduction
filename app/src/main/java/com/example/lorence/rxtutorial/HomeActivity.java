ca85e94b9dbd22e00840f29fac82d5ab64244005
lorence <lorence@enclave.vn>
2018-05-21 13:35:27 +0700

    Add img

--- app/src/main/java/com/example/lorence/rxtutorial/HomeActivity.java
+++ app/src/main/java/com/example/lorence/rxtutorial/HomeActivity.java
@@ -3,11 +3,15 @@ package com.example.lorence.rxtutorial;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import rx.Observable;
+
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Obser
     }
 }
