09791540f4d17dea30e12c714161dcd9b6dd9f64
lorence <lorence@enclave.vn>
2018-05-21 10:26:06 +0700

    Done

--- app/src/main/java/com/example/lorence/rxtutorial/HomeActivity.java
+++ app/src/main/java/com/example/lorence/rxtutorial/HomeActivity.java
@@ -1,7 +1,7 @@
 package com.example.lorence.rxtutorial;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
