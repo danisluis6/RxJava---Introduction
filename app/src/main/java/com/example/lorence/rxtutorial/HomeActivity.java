73484ad62dc2f08aff8fddfd27d227cc1181dd84
lorence <lorence@enclave.vn>
2018-05-21 17:30:10 +0700

    Done

--- app/src/main/java/com/example/lorence/rxtutorial/HomeActivity.java
+++ app/src/main/java/com/example/lorence/rxtutorial/HomeActivity.java
@@ -6,9 +6,13 @@ import android.support.v7.app.AppCompatActivity;
 import java.util.ArrayList;
 import java.util.List;
 
+import io.reactivex.Maybe;
+import io.reactivex.MaybeEmitter;
+import io.reactivex.MaybeOnSubscribe;
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.observers.DisposableObserver;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -17,7 +21,26 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        Observable<Node> observable = Observable.create(new ObservableOnSubscribe<Node>() {
+
+
+        Maybe way1 = (Maybe<List<Node>>) testGetNodes();
+        Maybe<List<Node>> way2 = Maybe.create(new MaybeOnSubscribe<List<Node>>() {
+            @Override
+            public void subscribe(MaybeEmitter<List<Node>> emitter) throws Exception {
+                try {
+                    List<Node> nodes = testGetNodes();
+                    if(nodes != null && !nodes.isEmpty()) {
+                        emitter.onSuccess(nodes);
+                    } else {
+                        emitter.onComplete();
+                    }
+                } catch (Exception e) {
+                    emitter.onError(e);
+                }
+            }
+        });
+
+        Observable<Node> observable =Observable.create(new ObservableOnSubscribe<Node>() {
             @Override
             public void subscribe(ObservableEmitter<Node> emitter) throws Exception {
                 try {
@@ -31,6 +54,27 @@ public class HomeActivity extends AppCompatActivity {
                 }
             }
         });
+
+        DisposableObserver<Node> disposable = observable.subscribeWith(new DisposableObserver<Node>() {
+            @Override
+            public void onNext(Node o) {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+
+        // Dispose the subscription when not interested in the emitted data any more
+        disposable.dispose();
+
     }
 
     private List<Node> testGetNodes() {
@@ -42,3 +86,10 @@ public class HomeActivity extends AppCompatActivity {
         return list;
     }
 }
+/**
+ * READ MORE:
+ * Link 1: http://www.vogella.com/tutorials/RxJava/article.html#disposing-subscriptions-and-using-compositedisposable
+ * Link 2: https://android.jlelse.eu/reactive-programming-for-android-d55bdbb438b4
+ * Link 3: https://medium.com/@fabioCollini/testing-asynchronous-rxjava-code-using-mockito-8ad831a16877
+ */
+
